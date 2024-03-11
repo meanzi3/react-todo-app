e160fd06ec50dffac98266a92bf6f33163c49437
meanzi3 <minjichoe0326@gmail.com>
2024-03-11 17:35:17 +0900

    refactor: Lists와 List 컴포넌트 분리

---
+++ src/components/List.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+function List({
+  id,
+  title,
+  completed,
+  todoData,
+  setTodoData,
+  provided,
+  snapshot,
+}) {
+  // X 버튼 클릭 이벤트 함수
+  const handleClick = (id) => {
+    let nowTodoData = todoData.filter((data) => data.id !== id);
+    setTodoData(nowTodoData);
+  };
+  // input checkbox change 이밴트 함수
+  const handleCompleteChange = (id) => {
+    let nowTodoData = todoData.map((data) => {
+      if (data.id === id) {
+        data.completed = !data.completed;
+      }
+      return data;
+    });
+    setTodoData(nowTodoData);
+  };
+  return (
+    <div
+      key={id}
+      {...provided.draggableProps}
+      ref={provided.innerRef}
+      {...provided.dragHandleProps}
+      className={`${
+        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+      } "flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded"`}
+    >
+      <div className="items-center">
+        <input
+          type="checkBox"
+          onChange={() => handleCompleteChange(id)}
+          defaultChecked={completed}
+        />
+        <span className={completed ? "line-through" : undefined}>{title}</span>
+      </div>
+      <div className="items-center">
+        <button
+          className="px-4 py-2 float-right"
+          onClick={() => handleClick(id)}
+        >
+          X
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default List;
--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,23 +1,8 @@
 import React from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import List from "./List";
 
-export default function List({ todoData, setTodoData }) {
-  // X 버튼 클릭 이벤트 함수
-  const handleClick = (id) => {
-    let nowTodoData = todoData.filter((data) => data.id !== id);
-    setTodoData(nowTodoData);
-  };
-  // input checkbox change 이밴트 함수
-  const handleCompleteChange = (id) => {
-    let nowTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    setTodoData(nowTodoData);
-  };
-
+export default function Lists({ todoData, setTodoData }) {
   const handleEnd = (result) => {
     // result 매개변수에는 soure 항목 및 대상 위치와 같은 드래그 이벤트에 대한 정보가 포함됨
     // 목적지가 없으면 (이벤트 취소) 해당 함수를 종료
@@ -48,38 +33,16 @@ export default function List({ todoData, setTodoData }) {
                   index={index}
                 >
                   {(provided, snapshot) => (
-                    <div
+                    <List
                       key={data.id}
-                      {...provided.draggableProps}
-                      ref={provided.innerRef}
-                      {...provided.dragHandleProps}
-                      className={`${
-                        snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-                      } "flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded"`}
-                    >
-                      <div className="items-center">
-                        <input
-                          type="checkBox"
-                          onChange={() => handleCompleteChange(data.id)}
-                          defaultChecked={data.completed}
-                        />
-                        <span
-                          className={
-                            data.completed ? "line-through" : undefined
-                          }
-                        >
-                          {data.title}
-                        </span>
-                      </div>
-                      <div className="items-center">
-                        <button
-                          className="px-4 py-2 float-right"
-                          onClick={() => handleClick(data.id)}
-                        >
-                          X
-                        </button>
-                      </div>
-                    </div>
+                      id={data.id}
+                      title={data.title}
+                      completed={data.completed}
+                      todoData={todoData}
+                      setTodoData={setTodoData}
+                      provided={provided}
+                      snapshot={snapshot}
+                    />
                   )}
                 </Draggable>
               ))}
